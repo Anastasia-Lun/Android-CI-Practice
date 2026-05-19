f7589c4ad8b7cf5fbc8f133edef903fb1933e186
Anastasia-Lun <anastasiabtv21@gmail.com>
2026-05-19 22:20:05 +0400

    Break code for failed build

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -17,13 +17,16 @@ import com.example.firstapp.ui.theme.FirstAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirstAppTheme {
-                GreetingScreen(
-                    message = "Hi, my name is Бородавка Анастасія"
-                )
+
+        val x =
+
+            setContent {
+                FirstAppTheme {
+                    GreetingScreen(
+                        message = "Hi, my name is Бородавка Анастасія"
+                    )
+                }
             }
-        }
     }
 }
 
