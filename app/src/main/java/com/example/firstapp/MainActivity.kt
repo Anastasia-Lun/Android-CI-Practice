bcc6b03c93d631a9a77cc64ac43d76dfebc7a398
Anastasia-Lun <anastasiabtv21@gmail.com>
2026-05-19 22:28:41 +0400

    Break code for failed build second try

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val x =
+        val x: String = 123
 
             setContent {
                 FirstAppTheme {
